1cfc09a2b8e1a4a528e899bd63e358686005321f
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-08 16:42:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Mr. Chocolate", 10, "male", "brown", "friendly", "true");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Rowdy", 1, "male", "black", "frisky", "false");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Tabitha", 5, "female", "brown", "friendly", "true");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Sergeant Brown", 10, "male", "brown", "mean", "true");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Melissa", 3, "female", "black", "friendly", "true");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Grinch", 4, "male", "brown", "mean", "false");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES ("Wendy", 10, "female", "black", "friendly", "true");
-INSERT INTO (name, age, gender, color, temperament, alive) FROM bears VALUES (NULL, 15, "male", "brown", "deadly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "male", "black", "frisky", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "female", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "mean", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "female", "black", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "male", "brown", "mean", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "female", "black", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 15, "male", "brown", "deadly", "true");
