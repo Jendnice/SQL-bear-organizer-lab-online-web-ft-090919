a52f91e3684655c1c1316ed4768651235516a166
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-08 17:31:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   # "SELECT color, COUNT(color) FROM bears GROUP BY color, COUNT(color);"
-  "SELECT color, MAX(count(color)) FROM bears"
+  "SELECT color, count(color) FROM bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
